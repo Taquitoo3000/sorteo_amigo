5b860d83e9264e84376a56cbf675948674a5589c
Taquitoo3000 <taquitodebistek23@gmail.com>
2025-08-19 14:28:22 -0600

    modificar app

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let lista = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
+let amigoSorteado;
+lista.innerHTML = "";
+
+function asignarTexto(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML=texto;
+    return;
+}
+
+function limpiar() {
+    document.getElementById("amigo").value = "";
+    resultado.innerHTML = "";
+}
+
+function agregarAmigo() {
+    let texto = document.getElementById("amigo");
+    if (texto.value.trim() == "") {
+        alert("Por favor, inserte un nombre");
+    }
+    else {
+        amigos.push(texto.value);
+        limpiar();
+        actualizarLista(amigos);
+        console.log(amigos);
+    }
+}
+
+function actualizarLista(amigos) {
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<br>${amigos[i]}`;
+    }   
+}
+
+function sortearAmigo() {
+    if (amigos.length<=1) {
+    alert("Añade al menos 2 amigos");
+    }
+    else {
+        amigoSorteado = amigos[parseInt(Math.floor(Math.random()*amigos.length))];
+        resultado.innerHTML =`Amigo Sorteado:<br>${amigoSorteado.toUpperCase()}`;
+    }
+}
